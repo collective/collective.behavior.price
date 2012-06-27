305a77dfb5d19997af4fe63295a930988abb5332
Taito Horiuchi <taito.horiuchi@gmail.com>
2012-06-27 21:34:44 +0300

    methods added.

--- collective/behavior/price/behavior.py
+++ collective/behavior/price/behavior.py
@@ -30,21 +30,34 @@ class Price(object):
     def price(self):
         return getattr(self.context, 'price', None)
 
+    def _set_price(self, value, name=''):
+        """Setting price as Decimal and money as Money for field name.
+
+        :param value: Price value such as 5.00, 5,00 nor 1800.
+        :type value: decimal.Decimal
+
+        :param name: Name of field.
+        :type name: str
+        """
+        if isinstance(value, Decimal):
+            # Set price
+            price_name = '{}price'.format(name)
+            setattr(self.context, price_name, value)
+            # Set money
+            money_name = '{}money'.format(name)
+            setattr(
+                self.context, money_name, Money(value, currency=self.currency))
+        else:
+            raise ValueError('Not Decimal.')
+
     @setproperty
     def price(self, value):
         """Setting price as Decimal and money as Money.
 
         :param value: Price value such as 5.00, 5,00 nor 1800.
-        :type value: str
+        :type value: decimal.Decimal
         """
-        if isinstance(value, Decimal):
-            # Set price
-            setattr(self.context, 'price', value)
-            # Set money
-            setattr(
-                self.context, 'money', Money(value, currency=self.currency))
-        else:
-            raise ValueError('Not Decimal.')
+        self._set_price(value)
 
     @property
     def currency(self):
@@ -55,6 +68,21 @@ class Price(object):
     def money(self):
         return getattr(self.context, 'money', None)
 
+    def _set_money(self, value, name=''):
+        """Setting money as Money.
+
+        :param value: Money instance.
+        :type value: moneyed.Money
+
+        :param name: Name of field.
+        :type name: str
+        """
+        if isinstance(value, Money):
+            money_name = '{}money'.format(name)
+            setattr(self.context, money_name, value)
+        else:
+            raise ValueError('Not Money.')
+
     @setproperty
     def money(self, value):
         """Setting money as Money.
@@ -62,7 +90,4 @@ class Price(object):
         :param value: Money instance.
         :type value: moneyed.Money
         """
-        if isinstance(value, Money):
-            setattr(self.context, 'money', value)
-        else:
-            raise ValueError('Not Money.')
+        self._set_money(value)
