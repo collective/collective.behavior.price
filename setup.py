c99f9a8268808ee8b008b6b350416c12eaad6ac0
Taito Horiuchi <taito.horiuchi@gmail.com>
2012-08-28 19:24:18 +0300

    Prepare collective.behavior.price 0.1.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ long_description = (
 
 setup(
     name='collective.behavior.price',
-    version='0.0',
+    version='0.1',
     description="Behavior to make content pricing.",
     long_description=long_description,
     # Get more strings from
